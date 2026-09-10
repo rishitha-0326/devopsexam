653fd260067732e3512f2b2adef6b6a1be0b2fe0
rishitha-0326 <rishithamadaraboina002@gmail.com>
2026-09-10 14:42:55 +0530

    my commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def register():
+    return render_template("register.html")
+
+@app.route("/success", methods=["POST"])
+def success():
+    username = request.form["username"]
+    email = request.form["email"]
+
+    return render_template("success.html", username=username, email=email)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5001, debug=True)
