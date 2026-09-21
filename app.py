d09adcff2354bcd86d47d2692df7066b1852fd0b
rishi <rishithamadaraboina002@gmail.com>
2026-09-21 10:47:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def success():
     return render_template("success.html", username=username, email=email)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
